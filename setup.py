68e46bcc842bf905421b9a4e451ac9ffed2f300a
ANAND VISHWAKARMA <anandvishwakarma21j@gmail.com>
2025-05-01 01:19:45 +0530

    structure of the project is ready and now going ahead with mongodb setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anand Kumar Vishwakarma",
+    author_email="anandvishwakarma21j@gmail.com",
+    packages=find_packages()
+)
